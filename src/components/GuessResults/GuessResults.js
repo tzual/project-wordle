b67a35dd0c5b57d85ae0182f93a674410645e761
Tzoor Rozental <rtzoor@gmail.com>
2023-12-27 15:31:17 +0200

    modify guess results to show a styled grid with predetermined rows and cells

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { range } from "../../utils.js";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants.js";
 
-function GuessResults({guessHistory}) {
-  return <ul className="guess-results">
-    {guessHistory.map((guess) => (
-      <li 
-        key={guess.id}>{guess.value}
-        class="guess"
-      </li>
-    ))}
-  </ul>;
+function GuessResults({ guessHistory }) {
+  const guessRows = range(NUM_OF_GUESSES_ALLOWED);
+  const guessCells = range(5);
+  
+  return (
+    <div className="guess-results">
+      {guessRows.map((guessIndex) => (
+        <p key={guessIndex} className="guess">
+          {guessCells.map((letterIndex) => (
+            <span key={letterIndex} className="cell">{guessHistory[guessIndex] &&
+                guessHistory[guessIndex].value[letterIndex]}</span>
+          ))}
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default GuessResults;
